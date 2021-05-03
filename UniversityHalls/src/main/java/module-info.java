4a28f8dd4fa7a48e7d15f1b870714ebbda372bd7
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-03 14:33:39 +0100

    Created University Hall project.

---
+++ UniversityHalls/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.universityhalls {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.universityhalls to javafx.fxml;
+    exports com.mycompany.universityhalls;
+}
+*/
