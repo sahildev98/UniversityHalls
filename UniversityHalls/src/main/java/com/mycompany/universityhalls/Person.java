e6684d7c119d94300a0f4be800d893a72d1a6099
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-04 16:03:38 +0100

    Implemented new instances. Added new instances to Constructor.

--- UniversityHalls/src/main/java/com/mycompany/universityhalls/Person.java
+++ UniversityHalls/src/main/java/com/mycompany/universityhalls/Person.java
@@ -15,14 +15,20 @@ public class Person {
     // Private variables created to only allow classes that inherits this class alone. 
     // Also to achieve escapsluation. 
     private String name;
+    private String gender;
+    private String nationality;
     private Date dateOfBirth;
     private String phoneNum;
     private int ID;
-    
-    public Person(String name, Date dateOfbirth, String phoneNum, int ID){
+    private String healthConditions;
+     // Constructor is a method used to initiaise objects i.e students entering details. 
+    public Person(String name,String gender, String nationality, Date dateOfbirth, String phoneNum, int ID, String healthConditions){
         this.name = name;
+        this.gender = gender;
+        this.nationality = nationality;
         this.dateOfBirth = dateOfbirth;
         this.phoneNum = phoneNum;
         this.ID = ID;
+        this.healthConditions = healthConditions;
     }   
 }
