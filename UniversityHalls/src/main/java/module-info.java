bcc13bca656947c1e0a2ec83adea79883a39974a
Sahil Ahmed <64647573+sahildev98@users.noreply.github.com>
2021-05-12 16:51:50 +0100

    Delete UniversityHalls directory
    Delete surpluse directory

--- UniversityHalls/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.universityhalls {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.universityhalls to javafx.fxml;
-    exports com.mycompany.universityhalls;
-}
-*/
