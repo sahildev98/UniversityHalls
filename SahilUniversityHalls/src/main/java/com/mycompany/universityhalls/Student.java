34407a5bc4f6c944b5831ecec58e9385fd981213
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-12 14:17:08 +0100

    Student class inherits Person class.

--- SahilUniversityHalls/src/main/java/com/mycompany/universityhalls/Student.java
+++ SahilUniversityHalls/src/main/java/com/mycompany/universityhalls/Student.java
@@ -5,10 +5,16 @@
  */
 package com.mycompany.universityhalls;
 
+import java.util.Date;
+
 /**
  *
  * @author Sahil
  */
-public class Student {
+public class Student extends Person {
+    
+    public Student(String name, String gender, String address, String nationality, Date dateOfbirth, String phoneNum, int ID, String healthConditions) {
+        super(name, gender, address, nationality, dateOfbirth, phoneNum, ID, healthConditions);
+    }
     
 }
