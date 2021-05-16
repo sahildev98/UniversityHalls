101fd615b15189037ad900acfbdec5ea4971c645
sahil <sahil.98@hotmail.com>
2021-05-16 17:42:31 +0100

    setters and getters implemented for yearOfStudy instance + javadoc comments.

--- SahilUniversityHalls/src/main/java/com/mycompany/universityhalls/Student.java
+++ SahilUniversityHalls/src/main/java/com/mycompany/universityhalls/Student.java
@@ -16,5 +16,19 @@ public class Student extends Person {
     public Student(String name, String gender, String address, String nationality, Date yearOfStudy, Date dateOfbirth, String phoneNum, int ID, String healthConditions) {
         super(name, gender, address, nationality, dateOfbirth, phoneNum, ID, healthConditions);
     }
+        /**
+     *
+     * @return the date of study. 
+     */
+    public Date getDate(){
+        return yearOfStudy;
+    }
     
+    /**
+     *
+     * @param newDate for setting the yearOfStudy date when creating a object for this class.  
+     */
+    public void setDate(Date newDate){
+        newDate = yearOfStudy;
+    }
 }
